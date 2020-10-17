593b9a8f5591e7655aebf97c9e444085e0928a6e
Alenafox <57696470+Alenafox@users.noreply.github.com>
2020-10-17 13:31:36 +0800

    Update main.kt

--- main.kt
+++ main.kt
@@ -2,19 +2,23 @@ package main
 
 import java.io.File
 
-fun readCOW(path: String): String{
+fun readCOW(path: String): MutableList<String> {
+    var arr: MutableList<String> = mutableListOf()
     val file = File(path)
-    val lines = file.readLines()
-    return lines.joinToString("")
+    for (line in file.readLines()){
+        for(word in line.split(" ","\n"))
+            arr.add(word)
+    }
+    return arr
 }
 
-fun eval(source: String){
+fun eval(source: MutableList<String>){
     val buffer = Array<Char> (200){ _ -> (0).toChar()}
     var ptr = 0 /* текущий указатель */
     var i = 0 //какой символ сейчас берем
-    while (i < source.length){
+    while (i < source.size ){
         when (source[i].toString()) {
-            "moO" -> ptr += 1
+            "moO" ->ptr += 1
             "mOo" -> ptr -= 1
             "MoO" -> buffer[ptr] = buffer[ptr] + 1
             "MOo" -> buffer[ptr] = buffer[ptr] - 1
@@ -22,8 +26,7 @@ fun eval(source: String){
                 if (buffer[ptr] == (0).toChar()) {
                     print(">>")
                     buffer[ptr] = readLine()?.toCharArray()?.get(0)!!
-                }
-                else {
+                } else {
                     print(buffer[ptr])
                 }
             }
@@ -35,7 +38,7 @@ fun eval(source: String){
 
 fun main(args: Array<String>){
     val source = readCOW("hello.cow")
-    println(source)
+    //println(source)
     println("Done!")
     eval(source)
 }
