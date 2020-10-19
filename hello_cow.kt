ec68db67e7609bf54bdfce8fb1815860026c79f9
Alenafox <57696470+Alenafox@users.noreply.github.com>
2020-10-19 15:03:30 +0800

    Update and rename main.kt to hello_cow.kt

--- hello_cow.kt
+++ hello_cow.kt
@@ -1,3 +1,6 @@
+//Done!
+//Hello, World!
+
 package main
 
 import java.io.File
@@ -6,15 +9,32 @@ fun readCOW(path: String): MutableList<String> {
     var arr: MutableList<String> = mutableListOf()
     val file = File(path)
     for (line in file.readLines()){
-        for(word in line.split(" ","\n"))
-            arr.add(word)
+        for(word in line.split(" "))
+            if(word.isNotEmpty())
+                arr.add(word)
     }
     return arr
+}
+
+fun getLoopBlocks(source: MutableList<String>): HashMap<Int, Int>{ //если цикл, то есть в сообщении скобки [.......]
+    val blocks = HashMap<Int, Int>()
+    val stack = mutableListOf<Int>()
+    for ((i, char) in source.withIndex()){
+        if (char == "MOO"){
+            stack.add(i)
+        }
+        if (char == "moo"){
+            blocks[i] = stack[stack.lastIndex]
+            blocks[stack.removeAt(stack.lastIndex)] = i
+        }
+    }
+    return blocks
 }
 
 fun eval(source: MutableList<String>){
     val buffer = Array<Char> (200){ _ -> (0).toChar()}
     var ptr = 0 /* текущий указатель */
+    val blocks = getLoopBlocks(source)
     var i = 0 //какой символ сейчас берем
     while (i < source.size ){
         when (source[i].toString()) {
@@ -30,15 +50,23 @@ fun eval(source: MutableList<String>){
                     print(buffer[ptr])
                 }
             }
+            "MOO" ->{
+                if (buffer[ptr] == (0).toChar()){
+                    i = blocks[i]!!
+                }
+            }
+            "moo" ->{
+                if (buffer[ptr] != (0).toChar()){
+                    i = blocks[i]!!
+                }
+            }
         }
         i += 1
     }
-    print(ptr)
 }
 
 fun main(args: Array<String>){
     val source = readCOW("hello.cow")
-    //println(source)
     println("Done!")
     eval(source)
 }
